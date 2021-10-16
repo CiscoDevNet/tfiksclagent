7ce57016540a1563963f61f175a8149165d5a099
prathjan <prathjan@cisco.com>
2021-10-15 21:17:18 -0700

    vars

--- scripts/getappd.sh
+++ scripts/getappd.sh
@@ -30,6 +30,10 @@ parse_input
 #. /home/ec2-user/environment/workshop/application.env
 #echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY > /tmp/accesskey
 
+download=zzz
+
+install=yyy
+echo -n "{\"download\":${download}, \"install\":${install}}" | tr -d ']['
 #echo -n "{\"download\":${download}, \"install\":${install}}" | tr -d ']['
-echo -n "{\"download\":aaa, \"install\":yyy }" | tr -d ']['
+#echo -n "{\"download\":aaa, \"install\":yyy }" | tr -d ']['
 #END
